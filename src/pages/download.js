d18d2934d839fc40e4cec82c1800f4422a2714c3
Ryan Kwon <kznm.develop@gmail.com>
2024-01-28 07:59:52 +0900

    Update download.js

--- src/pages/download.js
+++ src/pages/download.js
@@ -7,6 +7,7 @@ import styled from "styled-components";
 import { useNavigate } from "react-router-dom";
 import TX from "../style/Typography";
 
+
 export const Download = () => {
   const nav = useNavigate();
 

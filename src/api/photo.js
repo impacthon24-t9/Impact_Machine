1c0e9f98a9032b1f07ef3b3c5e51ac127828d052
Ryan Kwon <kznm.develop@gmail.com>
2024-01-28 09:04:14 +0900

    Update photo.js

--- src/api/photo.js
+++ src/api/photo.js
@@ -21,13 +21,12 @@ export const PostImage = async (url, fields, resultImg) => {
   form.append("policy", fields?.policy);
   if (Object.keys(fields).includes("x-amz-signature"))
     form.append("x-amz-signature", fields["x-amz-signature"]);
-  form.append("file", dataURLtoFile(resultImg, "image.png"));
-  form.append("Content-Type", "image/png");
-  form.append("Content-Disposition", "inline");
+  form.append("file", dataURLtoFile(resultImg, "image.jpeg"));
+  form.append("Content-Type", "image/jpeg");
 
   return await axios.post(url, form, {
     headers: {
-      "Content-Type": "image/png"
+      "Content-Type": "multipart/form-data"
     }
   });
 };
